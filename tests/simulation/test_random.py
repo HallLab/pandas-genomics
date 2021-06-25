6d4ab5a21a6adaef997193a999977d564936ecc4
John McGuigan <jrm5100@psu.edu>
2021-06-25 13:05:41 -0400

    Version 0.6.1, closing #23

--- tests/simulation/test_random.py
+++ tests/simulation/test_random.py
@@ -4,6 +4,6 @@ from pandas_genomics import sim
 
 def test():
     var = Variant(chromosome="1", position=123456, ref="T", alt=["A"])
-    gta = sim.generate_random_gt(var, allele_freq=[0.7, 0.3])
+    gta = sim.generate_random_gt(var, alt_allele_freq=0.3)
     var2 = Variant(chromosome="1", position=223456, ref="T", alt=["A", "C"])
-    gta_2 = sim.generate_random_gt(var2, allele_freq=[0.7, 0.25, 0.05])
+    gta_2 = sim.generate_random_gt(var2, alt_allele_freq=[0.25, 0.05])
